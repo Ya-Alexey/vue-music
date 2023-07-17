c79c1a6de75d499d3096a0ec28709bfb6aaadc7c
Ya-Alexey <y.alexey.a@mail.ru>
2023-07-17 21:51:46 +0300

    validation forms

--- src/main.js
+++ src/main.js
@@ -3,13 +3,16 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import VeeValidatePlugin from "@/includes/validation"
 
 import './assets/base.css'
 import './assets/main.css'
 
-const app = createApp(App)
+const app = createApp(App);
 
-app.use(createPinia())
-app.use(router)
+
+app.use(createPinia());
+app.use(router);
+app.use(VeeValidatePlugin);
 
 app.mount('#app')
